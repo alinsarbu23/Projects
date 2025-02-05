3abc8d6359795b45901bbebcc80da47aadcfb1e8
alinsarbu23 <147489942+alinsarbu23@users.noreply.github.com>
2025-02-05 12:34:22 +0200

    Eliminat submodul din Dame și adăugat fișierele în repository

---
+++ C#/Dame/Dame/Models/Piece.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Dame.Models
+{
+    public class Piece : INotifyPropertyChanged
+    {
+        private string name;
+        private PieceColor color;
+        private PieceType type;
+        private bool isKing;
+        private bool hasJumped;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
+
+        public PieceColor Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                NotifyPropertyChanged(nameof(Color));
+            }
+        }
+
+        public PieceType Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                NotifyPropertyChanged(nameof(Type));
+            }
+        }
+
+        public bool IsKing
+        {
+            get { return isKing; }
+            set
+            {
+                isKing = value;
+                NotifyPropertyChanged(nameof(IsKing));
+            }
+        }
+
+        public Piece(string name, PieceColor color, PieceType type, bool isKing)
+        {
+            Name = name;
+            Color = color;
+            Type = type;
+            IsKing = isKing;
+        }
+
+
+        public string GetPieceImagePath()
+        {
+            string cale = "C:\\Facultatea de Matematica si Informatica\\Anul 2\\Semestrul al doilea\\MVP\\Laborator\\L6\\Tema2\\Dame\\Resources\\";
+
+            switch (Color)
+            {
+                case PieceColor.Red:
+                    return IsKing ? cale + "pieceRedKing.png" : cale + "pieceRed.png";
+                case PieceColor.White:
+                    return IsKing ? cale + "pieceWhiteKing.png" : cale + "pieceWhite.png";
+                default:
+                    throw new ArgumentException("Invalid piece color.");
+            }
+        }
+
+
+        public bool HasJumped
+        {
+            get { return hasJumped; }
+            set
+            {
+                hasJumped = value;
+                NotifyPropertyChanged(nameof(HasJumped));
+            }
+        }
+
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+    }
+
+
+
+
+
+    public enum PieceColor
+    {
+        Red,
+        White,
+        None 
+    }
+
+
+    public enum PieceType
+    {
+        Regular,
+        King
+    }
+}
+
---
+++ C#/Dame/Dame/ViewModels/AboutInfoViewModel.cs
@@ -0,0 +1,72 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.IO;
+using System.Windows;
+
+namespace Dame.ViewModels
+{
+    public class AboutInfoViewModel : ViewModelBase
+    {
+        private string numeStudent;
+        private string adresaEmail;
+        private string grupa;
+        private string descriere;
+
+        public string StudentName
+        {
+            get { return numeStudent; }
+            set { Set(ref numeStudent, value); }
+        }
+
+        public string EmailAddress
+        {
+            get { return adresaEmail; }
+            set { Set(ref adresaEmail, value); }
+        }
+
+        public string Group
+        {
+            get { return grupa; }
+            set { Set(ref grupa, value); }
+        }
+
+        public string GameDescription
+        {
+            get { return descriere; }
+            set { Set(ref descriere, value); }
+        }
+
+        public RelayCommand LoadAboutInfoCommand { get; private set; }
+
+        public AboutInfoViewModel()
+        {
+            LoadAboutInfoCommand = new RelayCommand(LoadAboutInfo);
+        }
+
+        public void LoadAboutInfo()
+        {
+            try
+            {
+                string filePath = @"C:\Facultatea de Matematica si Informatica\Anul 2\Semestrul al doilea\MVP\Laborator\L6\Tema2\Dame\Resources\AboutInfo.txt";
+
+                string[] lines = File.ReadAllLines(filePath);
+
+                if (lines.Length >= 4)
+                {
+                    StudentName = lines[0];
+                    EmailAddress = lines[1];
+                    Group = lines[2];
+                    GameDescription = lines[3];
+                }
+                else
+                {
+                    MessageBox.Show("Fișierul AboutInfo.txt nu conține suficiente informații.", "Eroare", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"A apărut o eroare la citirea fișierului: {ex.Message}", "Eroare", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
