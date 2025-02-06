2b9766f57bbfdcfd8b3c00e6a147c8a34ec87204
alinsarbu23 <147489942+alinsarbu23@users.noreply.github.com>
2025-02-06 13:37:29 +0200

    Project Initialization

---
+++ C#/GameSquad/GameSquad/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GameSquad
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
